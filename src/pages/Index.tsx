9adcbf03dfb4bf3c40a0112aaf4859e7577ea13e
RajAbey68 <rajabey68@gmail.com>
2025-09-25 13:26:34 +0100

    Add src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Navigation } from "@/components/Navigation";
+import { DataDrivenHero } from "@/components/DataDrivenHero";
+import { Rooms } from "@/components/Rooms";
+import { Gallery } from "@/components/Gallery";
+import { DataDrivenAmenities } from "@/components/DataDrivenAmenities";
+import { DataDrivenLocation } from "@/components/DataDrivenLocation";
+import { KoLakeContact } from "@/components/KoLakeContact";
+import { KoLakeBooking } from "@/components/KoLakeBooking";
+
+const Index = () => {
+  const [bookingOpen, setBookingOpen] = useState(false);
+
+  const handleBookingClick = () => {
+    setBookingOpen(true);
+  };
+
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation onBookingClick={handleBookingClick} />
+      <main>
+        <DataDrivenHero onBookingClick={handleBookingClick} />
+        <Rooms onBookingClick={handleBookingClick} />
+        <Gallery />
+        <DataDrivenAmenities />
+        <DataDrivenLocation />
+        <KoLakeContact onBookingClick={handleBookingClick} />
+      </main>
+      <KoLakeBooking isOpen={bookingOpen} onOpenChange={setBookingOpen} />
+      
+    </div>
+  );
+};
+
+export default Index;
